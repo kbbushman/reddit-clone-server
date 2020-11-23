0adac42156fafacf7f5b9a670a51f70088e7680b
kbbushman <k.bushman@yahoo.com>
2020-11-22 23:18:15 -0800

    Adds User to mikro-orm entities config

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,16 +1,17 @@
 import path from 'path';
-import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import { MikroORM } from '@mikro-orm/core';
+import { Post } from "./entities/Post";
+import { User } from './entities/User';
+import { __prod__ } from "./constants";
 
 export default {
   migrations: {
     path: path.join(__dirname, './migrations'),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "redditclone",
   user: "kennybushman",
   type: "postgresql",
   debug: !__prod__,
-} as Parameters<typeof MikroORM.init>[0]; // This returns an array of accepted parameters for MikroORM.init()
+} as Parameters<typeof MikroORM.init>[0];
