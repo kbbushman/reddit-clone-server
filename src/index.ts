41e97e469e906ee1a62a2bcb1d5e3973ecf45e98
kbbushman <k.bushman@yahoo.com>
2020-11-22 23:50:09 -0800

    Adds user resolver

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { MikroORM } from '@mikro-orm/core';
 import mikroOrmConfig from './mikro-orm.config';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
+import { UserResolver } from './resolvers/user';
 
 
 const main = async () => {
@@ -16,7 +17,7 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
     context: () => ({ em: orm.em }),
