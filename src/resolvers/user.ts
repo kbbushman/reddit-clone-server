d2035fba682e89fff0a44739ca3cdda59fafb956
kbbushman <k.bushman@yahoo.com>
2020-11-23 00:20:36 -0800

    Adds user login mutation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -4,20 +4,42 @@ import {
   Arg,
   Field,
   InputType,
-  Ctx
+  Ctx,
+  ObjectType
 } from 'type-graphql';
 import argon2 from 'argon2';
 import { MyContext } from '../types';
 import { User } from '../entities/User';
 
+// InputType() decorator used for multiple input arguments
 // Alternative to adding each field to the @Arg() decorator
+// Not returned
 @InputType()
 class UsernamePasswordInput {
   @Field(() => String)
-  username: string
+  username: string;
 
   @Field(() => String)
-  password: string
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  
+  @Field()
+  message: string;
+}
+
+// ObjectType() decorator is returned
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], {nullable: true})
+  errors?: FieldError[];
+  
+  @Field(() => User, {nullable: true})
+  user?: User;
 }
 
 @Resolver()
@@ -37,5 +59,37 @@ export class UserResolver {
     await em.persistAndFlush(user);
 
     return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, {username: options.username});
+
+    if (!user) {
+      return {
+        errors: [{
+          field: 'username',
+          message: 'Username or password is incorrect'
+        }],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+
+    if (!valid) {
+      return {
+        errors: [{
+          field: 'password',
+          message: 'Username or password is incorrect'
+        }],
+      };
+    }
+
+    return {
+      user,
+    };
   }
 }
