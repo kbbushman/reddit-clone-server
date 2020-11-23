eacf7f91c4617531697106d4e8ba3fc6edc0d475
kbbushman <k.bushman@yahoo.com>
2020-11-22 20:46:36 -0800

    Adds Apollo Server with sanity check hello resolver

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 import { MikroORM } from '@mikro-orm/core';
-import { __prod__ } from './constants';
-import { Post } from './entities/Post';
 import mikroOrmConfig from './mikro-orm.config';
+import { HelloResolver } from './resolvers/hello';
 
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
 
-  // Test orm create
-  // const post = orm.em.create(Post, {title: 'Post One'});
-  // await orm.em.persistAndFlush(post);
+  const app = express();
 
-  // Test orm find
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => console.log('Server started on localhost:4000'));
 };
 
 main()
