796dc1461902f5f4bd702fcc8432e6de23f09a81
kbbushman <k.bushman@yahoo.com>
2020-11-22 20:09:29 -0800

    Adds main function with mikro-orm sanity check

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello Dave...');
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import mikroOrmConfig from './mikro-orm.config';
+
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up();
+
+  // Test orm create
+  // const post = orm.em.create(Post, {title: 'Post One'});
+  // await orm.em.persistAndFlush(post);
+
+  // Test orm find
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
+};
+
+main()
+  .catch((err) => console.log(err));
