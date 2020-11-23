e902ea002f13ba5318862618d5d0dc924da1d66d
kbbushman <k.bushman@yahoo.com>
2020-11-22 21:05:33 -0800

    Adds type-graphql Field decorator to expose columns to graphql schema

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType, Int } from "type-graphql";
 
+@ObjectType()
 @Entity() // Database Table Decorator
 export class Post {
+  @Field(() => Int) // Exposes column to graphql schema (omit to prevent queries on this property)
   @PrimaryKey() // PK Column Decorator
   id!: number;
 
+  @Field(() => String)
   @Property({type: 'date'}) // Column Decorator
   createdAt = new Date();
 
+  @Field(() => String)
   @Property({ type: 'date', onUpdate: () => new Date() })
   updatedAt = new Date();
 
+  @Field()
   @Property({type: 'text'})
   title!: string;
 }
