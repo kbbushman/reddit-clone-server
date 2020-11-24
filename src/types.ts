7f183cb368dc144dc818f2e78ac99a7d6e366f01
kbbushman <k.bushman@yahoo.com>
2020-11-23 20:55:56 -0800

    Updates request session type

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,9 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
 import { Request, Response } from 'express';
 
+
 export type MyContext = {
-  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
-  req: Request
-  res: Response
+  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+  req: Request & { session: any };
+  res: Response;
 };
