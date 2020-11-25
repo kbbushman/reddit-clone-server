c40ae4cd07970f1d59074d4cdb736ad15f9adc45
kbbushman <k.bushman@yahoo.com>
2020-11-24 22:49:39 -0800

    Replaces register orm create with orm createQueryBuilder

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,3 +1,4 @@
+import argon2 from 'argon2';
 import {
   Resolver,
   Mutation,
@@ -8,7 +9,7 @@ import {
   ObjectType,
   Query
 } from 'type-graphql';
-import argon2 from 'argon2';
+import { EntityManager } from '@mikro-orm/postgresql';
 import { MyContext } from '../types';
 import { User } from '../entities/User';
 
@@ -82,13 +83,21 @@ export class UserResolver {
 
     const hashedPassword = await argon2.hash(options.password);
 
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    });
+    let user;
 
     try {
-      await em.persistAndFlush(user);
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning('*');
+      
+      user = result[0];
     } catch (err) {
       // Duplicate username error
       if (err.code === '23505') {
