3ff6d6fb7903c58990f24e5c02e3bba95bdf2a68
kbbushman <k.bushman@yahoo.com>
2020-11-25 13:51:48 -0800

    Adds logout mutation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -12,6 +12,7 @@ import {
 import { EntityManager } from '@mikro-orm/postgresql';
 import { MyContext } from '../types';
 import { User } from '../entities/User';
+import { COOKIE_NAME } from '../constants';
 
 // InputType() decorator used for multiple input arguments
 // Alternative to adding each field to the @Arg() decorator
@@ -147,4 +148,20 @@ export class UserResolver {
 
     return { user };
   }
+
+  @Mutation(() => Boolean)
+  logout(
+    @Ctx() { req, res }: MyContext
+  ) {
+    return new Promise((resolve) => req.session.destroy((err) => {
+      res.clearCookie(COOKIE_NAME);
+      if (err) {
+        console.log(err);
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+    }));
+  }
+
 }
