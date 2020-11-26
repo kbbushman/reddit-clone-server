9a92a652b65bd376b829de514f915b721eca3e15
kbbushman <k.bushman@yahoo.com>
2020-11-26 15:31:20 -0800

    Removes user password reset token from redis after password change

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -55,7 +55,8 @@ export class UserResolver {
       };
     }
 
-    const userId = await redis.get(FORGET_PASSWORD_PREFIX + token);
+    const key = FORGET_PASSWORD_PREFIX + token;
+    const userId = await redis.get(key);
     if (!userId) {
       return { 
         errors: [
@@ -82,6 +83,9 @@ export class UserResolver {
 
     user.password = await argon2.hash(newPassword);
     await em.persistAndFlush(user);
+
+    // Delete user passwor reset from redis
+    await redis.del(key);
 
     // log in user after password change
     req.session.userId = user.id;
