9a2b1b8f43130614886496c3857c50c8133c7e42
kbbushman <k.bushman@yahoo.com>
2020-11-25 23:40:30 -0800

    Updates MyContext session type and adds redis

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,11 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
 import { Request, Response } from 'express';
-
+import { SessionData } from "express-session";
+import { Redis } from 'ioredis';
 
 export type MyContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-  req: Request & { session: { userId: number } };
+  req: Request & { session: SessionData & {userId: number} };
+  redis: Redis;
   res: Response;
 };
