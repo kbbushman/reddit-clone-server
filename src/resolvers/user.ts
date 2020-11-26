df6d480fa9a0cece9a65f72d867dbff8f78b69b6
kbbushman <k.bushman@yahoo.com>
2020-11-25 23:53:08 -0800

    Adds token, redis storage, and sendEmail to forgotPassword mutation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,5 @@
 import argon2 from 'argon2';
+import { v4 as uuidv4 } from 'uuid';
 import {
   Resolver,
   Mutation,
@@ -11,9 +12,10 @@ import {
 import { EntityManager } from '@mikro-orm/postgresql';
 import { MyContext } from '../types';
 import { User } from '../entities/User';
-import { COOKIE_NAME } from '../constants';
+import { COOKIE_NAME, FORGET_PASSWORD_PREFIX } from '../constants';
 import { UsernamePasswordInput } from './UsernamePasswordInput';
 import { validateRegister } from '../utils/validateRegister';
+import { sendEmail } from '../utils/sendEmail';
 
 @ObjectType()
 class FieldError {
@@ -39,9 +41,23 @@ export class UserResolver {
   @Mutation(() => Boolean)
   async forgotPassword(
     @Arg('email') email: string,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, redis }: MyContext
   ) {
-    // const user = await em.findOne(User, { email });
+    const user = await em.findOne(User, { email });
+    if (!user) {
+      // email not in database
+      return true;
+    }
+
+    const token = uuidv4();
+
+    await redis.set(FORGET_PASSWORD_PREFIX + token, user.id, 'ex', 1000 * 60 * 60 * 24 * 3); // 3 days
+
+    sendEmail(
+      email,
+      `<a href="http://localhost:3000/change-password/${token}">reset password</a>`
+    );
+
     return true;
   }
 
