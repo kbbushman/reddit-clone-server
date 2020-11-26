b7d246477ad7e517572ee21c408cc2bab4806d57
kbbushman <k.bushman@yahoo.com>
2020-11-25 23:39:48 -0800

    Replaces redis config with ioredis

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import express from 'express';
-import redis from 'redis';
+import Redis from 'ioredis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import cors from 'cors';
@@ -20,7 +20,7 @@ const main = async () => {
 
   const app = express();
   const RedisStore = connectRedis(session);
-  const redisClient = redis.createClient()
+  const redis = new Redis();
 
   app.use(cors({
     origin: 'http://localhost:3000',
@@ -31,7 +31,7 @@ const main = async () => {
     session({
       name: COOKIE_NAME,
       store: new RedisStore({
-        client: redisClient,
+        client: redis,
         disableTouch: true,
       }),
       cookie: {
@@ -51,7 +51,7 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({req, res}): MyContext => <MyContext>({ em: orm.em, req, res }),
+    context: ({req, res}): MyContext => <MyContext>({ em: orm.em, req, res, redis }),
   });
 
   apolloServer.applyMiddleware({
