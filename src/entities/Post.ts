8dee8ca7308f6a3cd9b7ccc3a35696ef4d082071
kbbushman <k.bushman@yahoo.com>
2020-11-27 00:00:50 -0800

    Refactors Post table to typeORM

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,22 +1,29 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
 import { Field, ObjectType, Int } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm";
 
 @ObjectType()
-@Entity() // Database Table Decorator
-export class Post {
-  @Field(() => Int) // Exposes column to graphql schema (omit to prevent queries on this property)
-  @PrimaryKey() // PK Column Decorator
+@Entity()
+export class Post extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
   id!: number;
 
   @Field(() => String)
-  @Property({type: 'date'}) // Column Decorator
-  createdAt = new Date();
+  @CreateDateColumn()
+  createdAt = Date;
 
   @Field(() => String)
-  @Property({ type: 'date', onUpdate: () => new Date() })
-  updatedAt = new Date();
+  @UpdateDateColumn()
+  updatedAt = Date;
 
-  @Field()
-  @Property({type: 'text'})
+  @Field(() => String)
+  @Column()
   title!: string;
 }
