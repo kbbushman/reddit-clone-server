0ace8edfadc18b2e6fffe52b1df78865d812d60d
kbbushman <k.bushman@yahoo.com>
2020-11-27 13:58:55 -0800

    Fixes isAuth middlware auto import path for MyContext

--- src/middleware/isAuth.ts
+++ src/middleware/isAuth.ts
@@ -1,5 +1,5 @@
-import { MyContext } from "src/types";
 import { MiddlewareFn } from "type-graphql";
+import { MyContext } from "../types";
 
 export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
   if (!context.req.session.userId) {
