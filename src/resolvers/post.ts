5b67a1522f861728fb861fac5d8b5cb5b04377b9
kbbushman <k.bushman@yahoo.com>
2020-11-27 00:02:24 -0800

    Refators Post queries and mutations to typeORM

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,51 +1,63 @@
 import { Post } from '../entities/Post';
-import { MyContext } from '../types';
-import { Resolver, Query, Ctx, Arg, Mutation } from 'type-graphql';
+import { Resolver, Query, Arg, Mutation } from 'type-graphql';
+import { getConnection } from 'typeorm';
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
-    return em.find(Post, {});
+  posts(): Promise<Post[]> {
+    return Post.find();
   }
 
   @Query(() => Post, {nullable: true})
-  post(
-    @Arg('id') id: number,
-    @Ctx() { em }: MyContext): Promise<Post | null> {
-    return em.findOne(Post, { id });
+  post(@Arg('id') id: number): Promise<Post | undefined> {
+    return Post.findOne(id);
   }
 
   @Mutation(() => Post)
-  async createPost(
-    @Arg('title') title: string,
-    @Ctx() { em }: MyContext): Promise<Post> {
-      const post = em.create(Post, { title });
-      await em.persistAndFlush(post);
+  async createPost(@Arg('title') title: string): Promise<Post> {
+    let post;
+
+    try {
+      const result = await getConnection()
+        .createQueryBuilder()
+        .insert()
+        .into(Post)
+        .values({
+          title: title,
+        })
+        .returning('*')
+        .execute();
+
+      post = result.raw[0];
+    } catch (error) {
       return post;
+    }
+
+    return post;
   }
 
   @Mutation(() => Post)
   async updatePost(
     @Arg('id') id: number,
-    @Arg('title', () => String, {nullable: true}) title: string,
-    @Ctx() { em }: MyContext): Promise<Post | null> {
-      const post = await em.findOne(Post, { id });
-      if (!post) {
-        return null;
-      }
-      if (typeof title !== 'undefined') {
-        post.title = title;
-        await em.persistAndFlush(post);
-      }
-      return post;
+    @Arg('title', () => String, {nullable: true}) title: string
+  ): Promise<Post | null> {
+    const post = await Post.findOne(id);
+
+    if (!post) {
+      return null;
+    }
+
+    if (typeof title !== 'undefined') {
+      await Post.update({ id }, { title });
+    }
+
+    return post;
   }
 
   @Mutation(() => Boolean)
-  async deletePost(
-    @Arg('id') id: number,
-    @Ctx() { em }: MyContext): Promise<boolean> {
-      await em.nativeDelete(Post, { id });
-      return true;
+  async deletePost(@Arg('id') id: number): Promise<boolean> {
+    await Post.delete(id);
+    return true;
   }
 }
