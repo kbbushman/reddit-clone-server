c9cb9549421c3eb0fa295c48288a387d3c9e0706
kbbushman <k.bushman@yahoo.com>
2020-11-26 20:03:03 -0800

    Removes mikroORM EntityManager property from MyContext type

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,3 @@
-import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
 import { Request, Response } from 'express';
 import { SessionData } from "express-session";
 import { Redis } from 'ioredis';
@@ -10,7 +9,6 @@ declare module 'express-session' {
 }
 
 export type MyContext = {
-  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
   req: Request & {session: SessionData};
   redis: Redis;
   res: Response;
